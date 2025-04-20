1c440da0d1752f5ac120aee2fc803277bfce22da
ikopmniko <hannapitman786@gmail.com>
2025-04-21 00:00:06 +0700

    Add files via upload

---
+++ run3.py
@@ -0,0 +1,110 @@
+import csv
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os
+import time
+import string
+from tmdbv3api import TMDb
+from tmdbv3api import Movie
+
+
+def random_string(count):
+    string.ascii_letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    return "".join(random.choice(string.ascii_letters) for x in range(count))
+
+    # return random.choice(string.ascii_letters)
+
+
+# Fungsi membaca CSV dengan rentang baris tertentu
+def read_csv_range(filename, start, end):
+    with open(filename, newline='', encoding='utf-8') as f:
+        rows = [row[0] for i, row in enumerate(csv.reader(f)) if start <= i < end]
+    return rows
+
+# Rentang data yang diproses (misal dari baris 1 sampai 50)
+start_row =0  # Baris pertama (0-based index)
+end_row = 200  # Baris terakhir yang ingin diproses
+
+# Deklarasi akun tunggal
+email = "zaleeinouraffr"
+password = "@@Eskepal123"
+
+# Baca judul video sesuai rentang yang diinginkan
+titles = read_csv_range("data.csv", start_row, end_row)
+
+# Inisialisasi WebDriver
+driver = webdriver.Chrome()
+driver.get("https://old.bitchute.com/channel/")
+time.sleep(3)
+
+# Login
+driver.find_element(By.CSS_SELECTOR, "input[type='text']").send_keys(email)
+time.sleep(1)
+driver.find_element(By.CSS_SELECTOR, "input[type='password']").send_keys(password)
+time.sleep(1)
+driver.find_element(By.CSS_SELECTOR, "button[type='button']").click()
+time.sleep(5)
+
+# Proses upload
+for title in titles:
+    try:
+
+        tmdbid = title
+        tmdb = TMDb()
+        tmdb.api_key = 'ce7dc231f477c13ba0789c8de9268db4'
+        tmdb.language = 'en-EN'
+        movie = Movie()
+        
+
+        
+        m = movie.details(tmdbid)
+        # poster_path = m.poster_path
+
+      
+        
+        year = m.release_date[0:4]
+        # poster = 'https://image.tmdb.org/t/p/w185/'+poster_path
+        link   ='https://flixzilla.site/movie/'+tmdbid+'?ref=bit'
+        judul  =m.title
+        original = m.original_title
+
+
+        modif_kata = title.replace(' ', '_')
+        kw = f'[NEW!] {judul} Movie Download Mp4moviez Filmyzilla ({random_string(5)})'
+
+        konten = f'''
+        29 minutes ago - Download {judul} Filmyzilla HD 720p 1080p
+
+        LINK ⏩⏩ {link}
+
+        {judul} filmyzilla hindi dubbed download filmyzilla
+
+        {judul}  hindi dubbed movie download mp4moviez 720p
+
+
+        '''
+
+        video_path = os.path.abspath("video/video.mp4")
+        
+        driver.get("https://old.bitchute.com/channel/")
+        time.sleep(3)
+        driver.find_element(By.CSS_SELECTOR, ".svg-inline--fa.fa-upload.fa-2x").click()
+        time.sleep(5)
+        driver.find_element(By.CSS_SELECTOR, "input[name='videoInput']").send_keys(video_path)
+        time.sleep(2)
+        driver.find_element(By.CSS_SELECTOR, "input[type='title']").send_keys(kw)
+        time.sleep(20)
+        driver.find_element(By.CSS_SELECTOR, "button[id='thumbnailButton']").click()
+        time.sleep(15)
+        driver.find_element(By.CSS_SELECTOR, "textarea[id='description']").send_keys(konten)
+        time.sleep(10)
+        driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+        time.sleep(7)
+        print("Berhasil Upload : "+title)
+
+    except:
+        print("TERJADI ERROR")
+        
+driver.quit()
